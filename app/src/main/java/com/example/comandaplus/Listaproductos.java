cb83d512f0547e2249bfb140e364325b8694aea0
vmanriquec <pool_rodrigo1@yahoo.com>
2019-02-13 15:04:12 -0500

    maestraproductos

--- app/src/main/java/com/example/comandaplus/Listaproductos.java
+++ app/src/main/java/com/example/comandaplus/Listaproductos.java
@@ -1,18 +1,525 @@
 package com.example.comandaplus;
-
-import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Listaproductos extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
+import com.example.comandaplus.adapter.Adaptadormaestraproducto;
+import com.example.comandaplus.modelo.Productos;
+import static android.content.Context.MODE_PRIVATE;
+
+public class Listaproductos  extends  AppCompatActivity implements   View.OnClickListener, RecyclerView.OnItemTouchListener {
+    private View view;
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lManager;
+    ArrayList<Productos> people=new ArrayList<>();
+    private String[] strArrData = {"No Suggestions"};
+
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+
+    ArrayList<String> mylist = new ArrayList<String>();
+    SharedPreferences prefs;
+    String face, FileName ="myfile" ,nombre,almacenactivosf,claveusuario,idalmacensf,idalmacenactivo,almacenactivo;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.layoutmaestrasproduc, container, false);
+
+        prefs =this.getSharedPreferences(FileName, MODE_PRIVATE);
+        face=prefs.getString("facebook","");
+
+        MultiAutoCompleteTextView myMultiAutoCompleteTextView
+                = (MultiAutoCompleteTextView)view.findViewById(
+                R.id.multiAutoCompleteTextView);
+// Obtener el Recycler PRODUCTOS
+        recycler = (RecyclerView) view.findViewById(R.id.maestraproductos);
+
+        int numberOfColumns = 6;
+        recycler.setHasFixedSize(true);
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+
+        myMultiAutoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String selected = (String) parent.getItemAtPosition(position);
+
+
+                new traerproductospornombre().execute(selected);
+            }
+        });
+
+        if (face.equals("si")){
+
+            nombre = prefs.getString("sessionnombre", "");
+            almacenactivo = prefs.getString("almacenactivo", "");
+            idalmacenactivo = prefs.getString("idalmacenactivo", "");
+
+            new llenarautocomplete().execute(idalmacenactivo);
+            new traerproductosporidalmacenidfamilia().execute("1");
+
+
+
+        }else if(face.equals("no")){
+            nombre = prefs.getString("nombreusuario", "");
+            almacenactivosf = prefs.getString("almacenactivosf", "");
+            claveusuario=prefs.getString("claveusuario","");
+            idalmacensf=prefs.getString("idalmacenactivosf","");
+
+            // Toast.makeText(HomeFragment.this.getActivity(),"login normal"+nombre+almacenactivosf+claveusuario+idalmacensf,Toast.LENGTH_LONG).show();
+
+
+
+
+        }
+
+
+        return view;
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.listadoproductos);
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        return false;
     }
 
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+    private class traerproductosporidalmacenidfamilia extends AsyncTask<String, String, String> {
+
+        HttpURLConnection conne;
+        URL url = null;
+        ArrayList<Productos> listaalmaceno = new ArrayList<Productos>();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                url = new URL("http://sodapop.space/sugest/apitraerproductosmaestra.php");
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+                conne = (HttpURLConnection) url.openConnection();
+                conne.setReadTimeout(READ_TIMEOUT);
+                conne.setConnectTimeout(CONNECTION_TIMEOUT);
+                conne.setRequestMethod("POST");
+                conne.setDoInput(true);
+                conne.setDoOutput(true);
+
+                // Append parameters to URL
 
 
 
+                Uri.Builder builder = new Uri.Builder()
+
+                        .appendQueryParameter("idalmacen", params[0])
+                        ;
+
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conne.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conne.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+            try {
+                int response_code = conne.getResponseCode();
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    InputStream input = conne.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+
+                    }
+                    return (
+
+                            result.toString()
+
+
+                    );
+
+                } else {
+                    return("Connection error");
+                }
+            } catch (IOException e) {
+                e.printStackTrace()                ;
+
+                return e.toString();
+            } finally {
+                conne.disconnect();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            people.clear();
+
+
+            ArrayList<String> dataList = new ArrayList<String>();
+            Productos meso;
+            if(result.equals("no rows")) {
+                Toast.makeText(getApplicationContext(),"no existen datos a mostrar",Toast.LENGTH_LONG).show();
+
+            }else{
+
+                try {
+
+
+                    JSONArray jArray = new JSONArray(result);
+
+
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject json_data = jArray.optJSONObject(i);
+
+                        meso = new Productos(json_data.getInt("idproducto"), json_data.getString("nombreproducto"), json_data.getString("estadoproducto"), json_data.getString("ingredientes"),json_data.getDouble(("precventa")),json_data.getString("descripcion"));
+                        people.add(meso);
+
+                    }
+                    strArrData = dataList.toArray(new String[dataList.size()]);
+
+
+                    adapter = new Adaptadormaestraproducto(people,getApplicationContext());
+                    recycler.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+                    recycler.setAdapter(adapter);
+
+
+                } catch (JSONException e) {
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private class llenarautocomplete extends AsyncTask<String, String, String> {
+
+        HttpURLConnection conne;
+        URL url = null;
+        ArrayList<Productos> listaalmaceno = new ArrayList<Productos>();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                url = new URL("http://sodapop.space/sugest/apitraertodoslosproductosporalmacen.php");
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+                conne = (HttpURLConnection) url.openConnection();
+                conne.setReadTimeout(READ_TIMEOUT);
+                conne.setConnectTimeout(CONNECTION_TIMEOUT);
+                conne.setRequestMethod("POST");
+                conne.setDoInput(true);
+                conne.setDoOutput(true);
+
+                // Append parameters to URL
+
+
+
+                Uri.Builder builder = new Uri.Builder()
+
+                        .appendQueryParameter("idalmacen", params[0])
+                        ;
+
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conne.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conne.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+            try {
+                int response_code = conne.getResponseCode();
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    InputStream input = conne.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+
+                    }
+                    return (
+
+                            result.toString()
+
+
+                    );
+
+                } else {
+                    return("Connection error");
+                }
+            } catch (IOException e) {
+                e.printStackTrace()                ;
+
+                return e.toString();
+            } finally {
+                conne.disconnect();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            people.clear();
+
+
+            ArrayList<String> dataList = new ArrayList<String>();
+            Productos meso;
+            if(result.equals("no rows")) {
+                Toast.makeText(getApplicationContext(),"no existen datos a mostrar",Toast.LENGTH_LONG).show();
+
+            }else{
+
+                try {
+
+
+                    JSONArray jArray = new JSONArray(result);
+
+
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject json_data = jArray.optJSONObject(i);
+
+                        meso = new Productos(json_data.getInt("idproducto"), json_data.getString("nombreproducto"), json_data.getString("estadoproducto"), json_data.getString("ingredientes"),json_data.getDouble(("precventa")),json_data.getString("descripcion"));
+                        people.add(meso);
+
+                        mylist.add(json_data.getString("nombreproducto"));
+
+                    }
+                    strArrData = dataList.toArray(new String[dataList.size()]);
+
+
+                    MultiAutoCompleteTextView myMultiAutoCompleteTextView
+                            = (MultiAutoCompleteTextView)view.findViewById(
+                            R.id.multiAutoCompleteTextView);
+                    myMultiAutoCompleteTextView.setAdapter(
+                            new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_dropdown_item_1line,mylist));
+                    myMultiAutoCompleteTextView.setTokenizer(
+                            new MultiAutoCompleteTextView.CommaTokenizer());
+
+
+
+                } catch (JSONException e) {
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private class traerproductospornombre extends AsyncTask<String, String, String> {
+
+        HttpURLConnection conne;
+        URL url = null;
+        ArrayList<Productos> listaalmaceno = new ArrayList<Productos>();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                url = new URL("http://sodapop.space/sugest/apitraerproductospornombre.php");
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+                conne = (HttpURLConnection) url.openConnection();
+                conne.setReadTimeout(READ_TIMEOUT);
+                conne.setConnectTimeout(CONNECTION_TIMEOUT);
+                conne.setRequestMethod("POST");
+                conne.setDoInput(true);
+                conne.setDoOutput(true);
+
+                // Append parameters to URL
+
+
+
+                Uri.Builder builder = new Uri.Builder()
+
+                        .appendQueryParameter("nombreproducto", params[0])
+                        ;
+
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conne.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conne.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+            try {
+                int response_code = conne.getResponseCode();
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    InputStream input = conne.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+
+                    }
+                    return (
+
+                            result.toString()
+
+
+                    );
+
+                } else {
+                    return("Connection error");
+                }
+            } catch (IOException e) {
+                e.printStackTrace()                ;
+
+                return e.toString();
+            } finally {
+                conne.disconnect();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            people.clear();
+
+
+            ArrayList<String> dataList = new ArrayList<String>();
+            Productos meso;
+            if(result.equals("no rows")) {
+                Toast.makeText(getApplicationContext(),"no existen datos a mostrar",Toast.LENGTH_LONG).show();
+
+            }else{
+                try {
+                    JSONArray jArray = new JSONArray(result);
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject json_data = jArray.optJSONObject(i);
+                        meso = new Productos(json_data.getInt("idproducto"), json_data.getString("nombreproducto"), json_data.getString("estadoproducto"), json_data.getString("ingredientes"),json_data.getDouble(("precventa")),json_data.getString("descripcion"));
+                        people.add(meso);
+                    }
+                    strArrData = dataList.toArray(new String[dataList.size()]);
+                    recycler.removeAllViews();recycler.setAdapter(null);
+                    adapter = new Adaptadormaestraproducto(people,getApplication().getApplicationContext());
+                    recycler.setLayoutManager(new GridLayoutManager(getApplicationContext(), 1));
+                    recycler.setAdapter(adapter);
+                } catch (JSONException e) {
+
+                }
+            }
+        }
+    }
 }
+
